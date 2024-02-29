0824be8566964d0d76e0ea5eefa887c904306edb
notmhmd <mhmdfrj.97@gmail.com>
2024-02-29 21:32:42 +0400

    remove map

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -3,27 +3,24 @@ import CssBaseline from '@mui/joy/CssBaseline';
 import Box from '@mui/joy/Box';
 import Stack from '@mui/joy/Stack';
 import { Filters, Header, Navbar, Search, Pagination, ItemCard } from "../../components";
-import mapboxgl from "mapbox-gl";
-import Map, { GeolocateControl, Marker } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
-import { useCallback, useEffect, useRef, useState } from "react";
-import Config from "../../config";
-import { GetServiceParams, Service, useServicesList } from "../../api";
+import { useEffect, useState } from "react";
+import { GetServiceParams, useServicesList } from "../../api";
 import { CircularProgress } from "@mui/joy";
 import { CountryType } from "../../components/CountrySelector/CountrySelector.tsx";
 
 export default function Home() {
   const [params, setParams] = useState<GetServiceParams>({});
   const {isLoading, data:services, refetch} = useServicesList(params)
-  const mapRef = useRef(null);
-  const geoControlRef = useCallback((ref:mapboxgl.GeolocateControl) => {
-    if (ref) {
-      (async () => {
-        while (!mapRef.current) await ((() => new Promise((resolve) => setTimeout(resolve, 200)))())
-        ref.trigger();
-      })()
-    }
-  }, []);
+  // const mapRef = useRef(null);
+  // const geoControlRef = useCallback((ref:mapboxgl.GeolocateControl) => {
+  //   if (ref) {
+  //     (async () => {
+  //       while (!mapRef.current) await ((() => new Promise((resolve) => setTimeout(resolve, 200)))())
+  //       ref.trigger();
+  //     })()
+  //   }
+  // }, []);
 
 
   const renderItems = () =>{
@@ -76,9 +73,9 @@ export default function Home() {
     }));
   }
 
-   const popup = ((service:Service) => {
-    return new mapboxgl.Popup().setText(service.description);
-  })
+  //  const popup = ((service:Service) => {
+  //   return new mapboxgl.Popup().setText(service.description);
+  // })
 
 
   return (
@@ -106,42 +103,42 @@ export default function Home() {
           <Header />
           <Search onSearch={handleSearch} />
         </Stack>
-        {<Box
-          sx={{
-            gridRow: "span 3",
-            display: { md: "flex" },
-            height: { xs: "25vh", md:"auto" }
+        {/*{<Box*/}
+        {/*  sx={{*/}
+        {/*    gridRow: "span 3",*/}
+        {/*    display: { md: "flex" },*/}
+        {/*    height: { xs: "25vh", md:"auto" }*/}
 
-          }}
-        >
-          <Map
-            mapboxAccessToken={Config.MAPBOX_KEY}
-            ref={mapRef}
-            initialViewState={{
-              longitude: -100,
-              latitude: 40,
-              zoom: 3.5,
-            }}
-            mapStyle="mapbox://styles/mapbox/streets-v11"
-          >
-            <GeolocateControl
-              positionOptions={{ enableHighAccuracy: true }}
-              trackUserLocation={true}
-              showUserLocation={true}
-              ref={geoControlRef}
-            />
+        {/*  }}*/}
+        {/*>*/}
+        {/*  <Map*/}
+        {/*    mapboxAccessToken={Config.MAPBOX_KEY}*/}
+        {/*    ref={mapRef}*/}
+        {/*    initialViewState={{*/}
+        {/*      longitude: -100,*/}
+        {/*      latitude: 40,*/}
+        {/*      zoom: 3.5,*/}
+        {/*    }}*/}
+        {/*    mapStyle="mapbox://styles/mapbox/streets-v11"*/}
+        {/*  >*/}
+        {/*    <GeolocateControl*/}
+        {/*      positionOptions={{ enableHighAccuracy: true }}*/}
+        {/*      trackUserLocation={true}*/}
+        {/*      showUserLocation={true}*/}
+        {/*      ref={geoControlRef}*/}
+        {/*    />*/}
 
-            {services && services.services.map((service) => (
-              <Marker
-                key={service.id}
-                longitude={service.lan}
-                latitude={service.lat}
-                popup={popup(service)}
-              >
-              </Marker>
-            ))}
-          </Map>
-        </Box>}
+        {/*    {services && services.services.map((service) => (*/}
+        {/*      <Marker*/}
+        {/*        key={service.id}*/}
+        {/*        longitude={service.lan}*/}
+        {/*        latitude={service.lat}*/}
+        {/*        popup={popup(service)}*/}
+        {/*      >*/}
+        {/*      </Marker>*/}
+        {/*    ))}*/}
+        {/*  </Map>*/}
+        {/*</Box>}*/}
         <Stack spacing={2} sx={{ px: { xs: 2, md: 4 }, pt: 2, minHeight: 0 }}>
           <Filters onCountryChange={handleCityChange} onRangeChange={handleRangeChange} />
           <Stack spacing={2} sx={{ overflow: 'auto' }}>
